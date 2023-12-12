fab982ad9da2695291ed2fe23929df2df20138cd
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2023-12-13 00:19:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@
 
             let Text = "== Shelshog Voicechat ==</br>"
             for (const i of Object.keys(KeyBindings))
-            Text+=`</br>[${i}] to ${KeyBindings[i][0]}`
+                Text+=`</br>[${i}] to ${KeyBindings[i][0]}`
             DisplayMessage(Help, Text)
         }]
     }
@@ -74,60 +74,60 @@
 
 
     const Alpha = function(){
-        const Upper = Array(26).fill(1).map((_, i)=>String.fromCharCode(65+i)),
-        Lower = Upper.map(i=>i.toLowerCase())
-            return {Lower: Lower, Upper: Upper}
-        }()
+    const Upper = Array(26).fill(1).map((_, i)=>String.fromCharCode(65+i)),
+    Lower = Upper.map(i=>i.toLowerCase())
+        return {Lower: Lower, Upper: Upper}
+    }()
 
-        const Font = {
-            Lower: ['𝖺','𝖻','𝖼','𝖽','𝖾','𝖿','𝗀','𝗁','𝗂','𝗃','𝗄','𝗅','𝗆','𝗇','𝗈','𝗉','𝗊','𝗋','𝗌','𝗍','𝗎','𝗏','𝗐','𝗑','𝗒','𝗓'],
-            Upper: ['𝖠','𝖡','𝖢','𝖣','𝖤','𝖥','𝖦','𝖧','𝖨','𝖩','𝖪','𝖫','𝖬','𝖭','𝖮','𝖯','𝖰','𝖱','𝖲','𝖳','𝖴','𝖵','𝖶','𝖷','𝖸','𝖹']
+    const Font = {
+        Lower: ['𝖺','𝖻','𝖼','𝖽','𝖾','𝖿','𝗀','𝗁','𝗂','𝗃','𝗄','𝗅','𝗆','𝗇','𝗈','𝗉','𝗊','𝗋','𝗌','𝗍','𝗎','𝗏','𝗐','𝗑','𝗒','𝗓'],
+        Upper: ['𝖠','𝖡','𝖢','𝖣','𝖤','𝖥','𝖦','𝖧','𝖨','𝖩','𝖪','𝖫','𝖬','𝖭','𝖮','𝖯','𝖰','𝖱','𝖲','𝖳','𝖴','𝖵','𝖶','𝖷','𝖸','𝖹']
+    }
+
+    function GetText(Text){
+        Object.keys(Buzzwords).sort().reverse().forEach(i=>Text = Text.replace(i, Buzzwords[i]))
+
+        Text = Text.split("")
+        for (let i=0;i<Text.length;i++){
+            const Case = ["Lower", "Upper"].filter(c=>Alpha[c].includes(Text[i]))[0]
+            if (!Case) continue
+            Text[i] = Font[Case][Alpha[Case].indexOf(Text[i])]
         }
 
-        function GetText(Text){
-            Object.keys(Buzzwords).sort().reverse().forEach(i=>Text = Text.replace(i, Buzzwords[i]))
+        return Text.join('')
+    }
 
-            Text = Text.split("")
-            for (let i=0;i<Text.length;i++){
-                const Case = ["Lower", "Upper"].filter(c=>Alpha[c].includes(Text[i]))[0]
-                if (!Case) continue
-                Text[i] = Font[Case][Alpha[Case].indexOf(Text[i])]
-            }
+    if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
+        alert("shelshog voicechat started")
 
-            return Text.join('')
+        const Client = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+        Client.lang = 'en-US';
+        Client.interimResults = false
+        Client.continuous = true
+        Client.maxAlternatives = 1
+
+        Client.onresult = (event) => {
+            if (!CanRecord) return
+
+            const Div = document.querySelector(".chat-container")?.childNodes[2]
+            if (!Div) return
+
+            Div.click()
+            Div.value = GetText(event.results[event.results.length-1][0].transcript.toLowerCase())
+            Div.dispatchEvent(new KeyboardEvent('keydown', {
+                key: 'Enter',
+                keyCode: 13,
+                which: 13,
+                bubbles: true,
+                cancelable: true,
+            }))
         }
 
-        if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
-            alert("shelshog voicechat started")
+        Client.onend = ()=> Client.start()
 
-            const Client = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-            Client.lang = 'en-US';
-            Client.interimResults = false
-            Client.continuous = true
-            Client.maxAlternatives = 1
-
-            Client.onresult = (event) => {
-                if (!CanRecord) return
-
-                const Div = document.querySelector(".chat-container")?.childNodes[2]
-                if (!Div) return
-
-                Div.click()
-                Div.value = GetText(event.results[event.results.length-1][0].transcript.toLowerCase())
-                Div.dispatchEvent(new KeyboardEvent('keydown', {
-                    key: 'Enter',
-                    keyCode: 13,
-                    which: 13,
-                    bubbles: true,
-                    cancelable: true,
-                }))
-            }
-
-            Client.onend = ()=> Client.start()
-
-            Client.start()
-        }
-        else
+        Client.start()
+    }
+    else
         alert('shelshog voicechat not supported')
 
 
